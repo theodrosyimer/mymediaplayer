5f6f316fff25d52574cb8d434c239301cd16d263
Theodros Yimer <theox01@hotmail.fr>
2023-03-21 14:20:42 +0100

    feat(index.js): add custom controls

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
 const supportsVideo = !!document.createElement("video").canPlayType
 if (supportsVideo) {
   console.log('video element supported!')
+  const videoContainer = document.getElementById("video-container")
+  const video = document.getElementById("video")
+  const videoControls = document.getElementById("video-controls")
+
+  // Hide the default controls
+  video.controls = false
+
+  // Display the user defined video controls
+  videoControls.style.display = "flex"
 }
