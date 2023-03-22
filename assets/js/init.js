9cf51e259c7c541be14b0c9551c886cad37cda2c
Theodros Yimer <theox01@hotmail.fr>
2023-03-22 23:27:09 +0100

    feat: add dynamic rendering

---
+++ assets/js/init.js
@@ -0,0 +1,173 @@
+import { appendChildren, batchCreateElement, createElement } from "./utils.js"
+
+const video = createElement({
+  tag: 'video',
+  options: {
+    id: 'video',
+    controls: '',
+    preload: 'metadata',
+    poster: 'assets/videos/how-to-google-it-like-a-senior-developer/poster.png',
+  }
+})
+
+const source = createElement({
+  tag: 'source',
+  options: {
+    type: 'video/mp4',
+    src: 'assets/videos/how-to-google-it-like-a-senior-developer/how-to-google-it-like-a-senior-developer.mp4'
+  }
+})
+
+const videoLink = createElement({
+  tag: 'a',
+  options: {
+    text: 'Download MP4',
+    href: 'assets/videos/how-to-google-it-like-a-senior-developer/how-to-google-it-like-a-senior-developer.mp4',
+  }
+})
+
+const videoControls = createElement({
+  tag: 'div',
+  options: {
+    id: 'video-controls',
+    class: ['controls', 'grid'],
+  }
+})
+
+const soundControls = createElement({
+  tag: 'div',
+  options: {
+    id: 'sound-controls',
+    class: ['sound-controls', 'flex', 'test']
+  }
+})
+
+const soundControlsButtonsObject = [
+  {
+    tag: 'button',
+    options: {
+      id: 'mute',
+      class: ['button', 'icon', 'fa-mute'],
+      // text: 'Mute/Unmute'
+    }
+  },
+  {
+    tag: 'button',
+    options: {
+      id: 'volume-decrement',
+      class: ['button', 'icon', 'fa-vol-dec'],
+      // text: '↓'
+    }
+  },
+  {
+    tag: 'input',
+    options: {
+      id: 'volume-control',
+      type: 'range',
+      min: '0',
+      max: '100',
+      step: '1',
+      // class: [],
+      // text: '↓'
+    }
+  },
+  {
+    tag: 'button',
+    options: {
+      id: 'volume-increment',
+      class: ['button', 'icon', 'fa-vol-inc'],
+      // text: '↑'
+    }
+  }
+]
+
+const soundControlsButtons = batchCreateElement(soundControlsButtonsObject, createElement)
+
+const mainControls = createElement({
+  tag: 'div',
+  options: {
+    class: ['main-controls', 'flex']
+  }
+})
+
+const mainControlsButtonsObject = [
+  {
+    tag: 'button',
+    options: {
+      id: 'backward',
+      class: ['button', 'icon', 'fa-backward'],
+      // text: '←'
+    }
+  },
+  {
+    tag: 'button',
+    options: {
+      id: 'playpause',
+      class: ['button', 'icon', 'fa-play'],
+      // text: 'Play/Pause'
+    }
+  },
+  {
+    tag: 'button',
+    options: {
+      id: 'forward',
+      class: ['button', 'icon', 'fa-forward'],
+      // text: '→'
+    }
+  }
+]
+
+const mainControlsButtons = batchCreateElement(mainControlsButtonsObject, createElement)
+
+const screenControls = createElement({
+  tag: 'div',
+  options: {
+    id: 'screen-controls',
+    class: ['screen-controls'],
+  }
+})
+
+const screenControlsButtons = createElement({
+  tag: 'button',
+  options: {
+    id: 'fullscreen',
+    class: ['button', 'icon', 'fa-fullscreen'],
+    // text: 'Fullscreen'
+  }
+})
+
+const progress = createElement({
+  tag: 'div',
+  options: {
+    class: 'progress'
+  }
+})
+
+
+const progressBar = createElement({
+  tag: 'progress',
+  options: {
+    id: 'progress',
+    value: '0',
+    min: '0'
+  }
+})
+
+const spanProgressBar = createElement({
+  tag: 'span',
+  options: {
+    id: 'progress-bar'
+  }
+})
+
+export function initUI() {
+  const videoContainer = document.querySelector("#video-container")
+  appendChildren(videoContainer, [video, videoControls])
+  appendChildren(video, [source, videoLink])
+  appendChildren(videoControls, [soundControls, mainControls, screenControls, progress])
+  appendChildren(soundControls, soundControlsButtons)
+  appendChildren(mainControls, mainControlsButtons)
+  appendChildren(screenControls, [screenControlsButtons])
+  appendChildren(progress, [progressBar])
+  appendChildren(progressBar, [spanProgressBar])
+}
---
+++ assets/js/video-player-features.js
@@ -0,0 +1,168 @@
+import { createBatchFn } from "./utils.js"
+
+const NAVIGATION_TIME = 3
+const VOLUME_RATIO = 0.1
+const videoContainer = document.querySelector("#video-container")
+
+let features = [
+  {
+    name: 'video',
+    event: 'click',
+    element: () => document.querySelector("#video"),
+    action: () => {
+      if (video.paused || video.ended) {
+        video.play()
+      } else {
+        video.pause()
+      }
+    }
+  },
+  {
+    name: 'video',
+    event: 'timeupdate',
+    element: () => document.querySelector("#video"),
+    action: () => {
+      if (!document.querySelector("#progress").getAttribute("max"))
+        document.querySelector("#progress").setAttribute("max", video.duration)
+      document.querySelector("#progress").value = video.currentTime
+      document.querySelector("#progress-bar").style.width = `${Math.floor(
+        (video.currentTime * 100) / video.duration
+      )}%`
+    }
+  },
+  {
+    name: 'playpause',
+    event: 'click',
+    element: () => document.querySelector("#playpause")
+    ,
+    action: () => {
+      if (video.paused || video.ended) {
+        video.play()
+        document.querySelector("#playpause").classList.remove('fa-play')
+        document.querySelector("#playpause").classList.add('fa-pause')
+      } else {
+        video.pause()
+        document.querySelector("#playpause").classList.remove('fa-pause')
+        document.querySelector("#playpause").classList.add('fa-play')
+      }
+    }
+  },
+  {
+    name: 'backward',
+    event: 'click',
+    element: () => document.querySelector("#backward"),
+    action: () => {
+      video.currentTime = video.currentTime - NAVIGATION_TIME
+    }
+  },
+  {
+    name: 'forward',
+    event: 'click',
+    element: () => document.querySelector("#forward"),
+    action: () => {
+      video.currentTime = video.currentTime + NAVIGATION_TIME
+    }
+  },
+  {
+    name: 'volumeIncrement',
+    event: 'click',
+    element: () => document.querySelector("#volume-increment"),
+    action: () => {
+      if (video.volume < 1) {
+        video.volume = video.volume + VOLUME_RATIO
+      }
+    }
+  },
+  {
+    name: 'volumeDecrement',
+    event: 'click',
+    element: () => document.querySelector("#volume-decrement"),
+    action: () => {
+      if (video.volume > 0.1) {
+        video.volume = video.volume - VOLUME_RATIO
+      }
+    }
+  },
+  {
+    name: 'mute',
+    event: 'click',
+    element: () => document.querySelector("#mute"),
+    action: () => {
+      video.muted = !video.muted
+    }
+  },
+  {
+    name: 'fullscreen',
+    event: 'click',
+    element: () => document.querySelector("#fullscreen"),
+    action: () => {
+      handleFullscreen()
+    }
+  },
+  {
+    name: 'skipAhead',
+    event: 'click',
+    element: () => document.querySelector("#progress"),
+    action: (e) => {
+      const rect = document.querySelector("#progress").getBoundingClientRect()
+      const pos = (e.pageX - rect.left) / document.querySelector("#progress").offsetWidth
+      video.currentTime = pos * video.duration
+    }
+  },
+  {
+    name: 'volume-control',
+    event: 'input',
+    element: () => document.querySelector("#volume-control"),
+    action: (e) => {
+      const video = document.querySelector("#video")
+      video.volume = e.target.value / 100
+    }
+  }
+]
+
+export const initVideoPlayerFeatures = createBatchFn(features, initFeature)
+
+function initFeature({ element, event, action }) {
+  element().addEventListener(event, action)
+}
+
+
+// Set the video container's fullscreen state
+function setFullscreenData(state) {
+  videoContainer.setAttribute('data-fullscreen', !!state)
+}
+
+// Checks if the document is currently in fullscreen mode
+function isFullScreen() {
+  return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement)
+}
+
+// Fullscreen
+function handleFullscreen() {
+  // If fullscreen mode is active...
+  if (isFullScreen()) {
+    // ...exit fullscreen mode
+    // (Note: this can only be called on document)
+    if (document.exitFullscreen) document.exitFullscreen()
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen()
+    else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen()
+    else if (document.msExitFullscreen) document.msExitFullscreen()
+    setFullscreenData(false)
+  }
+  else {
+    // ...otherwise enter fullscreen mode
+    // (Note: can be called on document, but here the specific element is used as it will also ensure that the element's children, e.g. the custom controls, go fullscreen also)
+    if (videoContainer.requestFullscreen) videoContainer.requestFullscreen()
+    else if (videoContainer.mozRequestFullScreen) videoContainer.mozRequestFullScreen()
+    else if (videoContainer.webkitRequestFullScreen) {
+      // Safari 5.1 only allows proper fullscreen on the video element. This also works fine on other WebKit browsers as the following CSS (set in styles.css) hides the default controls that appear again, and
+      // ensures that our custom controls are visible:
+      // figure[data-fullscreen=true] video::-webkit-media-controls { display:none !important; }
+      // figure[data-fullscreen=true] .controls { z-index:2147483647; }
+      video.webkitRequestFullScreen()
+    }
+    else if (videoContainer.msRequestFullscreen) videoContainer.msRequestFullscreen()
+    setFullscreenData(true)
+  }
+}
+
